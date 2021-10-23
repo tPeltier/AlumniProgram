e016e76a03948ad975b7e842907d4d554d81b799
tPeltier <blackeagle393@gmail.com>
2021-10-23 15:32:16 -0400

    removedDemo

--- src/App.java
+++ src/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] group3) {
         System.out.println("Print Your Name Below:");
         System.out.println("Timothy");
-        System.out.println("Andrew");
     }
 }
