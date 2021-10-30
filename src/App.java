449fad7a671c51c2c39a101161c3020da7bcb3a1
tPeltier <blackeagle393@gmail.com>
2021-10-30 13:41:35 -0400

    added file read/write

--- src/App.java
+++ src/App.java
@@ -1,7 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] group3) {
+    public static void main(String[] group3) throws FileNotFoundException{
+        File inputFile = new File("input.txt");
+        Scanner fileIn = new Scanner(inputFile);
+        PrintWriter saved = new PrintWriter("saved.txt");
+        LinkedList names = new LinkedList<>();
         System.out.println("Print Your Name Below:");
         System.out.println("Timothy");
         System.out.println("Andrew");
+
+        System.out.println("Expected Name List:");
+        while (fileIn.hasNextLine()) {
+            String s = fileIn.nextLine();
+            names.add(s);
+            System.out.println(s);
+        }
+        saved.println(names.toString());
+        saved.close();
+        fileIn.close();
     }
 }
