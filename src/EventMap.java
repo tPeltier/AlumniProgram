a4dacaafd5b3384b89253311f67f1f0a7c74f818
slater58 <aslater3122@gmail.com>
2021-10-30 19:08:16 -0400

    added map classes

---
+++ src/EventMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class EventMap {
+    HashMap<Integer, Object> event;
+
+    public EventMap() {
+    }
+
+    public void constructEventMap() {
+
+    }
+
+    public void editEventMap() {
+
+    }
+
+    public HashMap<Integer, Object> getEvent() {
+        return event;
+    }
+}
