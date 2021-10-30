39c1cfb9b121f2010d538397878212226fdf35ae
slater58 <aslater3122@gmail.com>
2021-10-30 19:43:18 -0400

    added java docs

--- src/EventMap.java
+++ src/EventMap.java
@@ -3,17 +3,30 @@ import java.util.HashMap;
 public class EventMap {
     HashMap<Integer, Object> event;
 
+    /**
+     * constructs an object for and event map
+     */
     public EventMap() {
     }
 
+    /**
+     * assigns the initial values to the event map
+     */
     public void constructEventMap() {
 
     }
 
+    /**
+     * edits and adds to event map
+     */
     public void editEventMap() {
 
     }
 
+    /**
+     *
+     * @return returns the event map;
+     */
     public HashMap<Integer, Object> getEvent() {
         return event;
     }
