5be02d51e13745bca99eeb03adb7271b7c2821b7
steakmeat197 <88911660+steakmeat197@users.noreply.github.com>
2021-11-08 11:38:55 -0500

    Update Events.java

--- src/Events.java
+++ src/Events.java
@@ -1,3 +1,72 @@
 public class Events {
-    
+
+    String name;
+    String time;
+    int room;
+    int numberOfParticipants;
+    String startDate;
+
+
+
+    // constructors 
+    public Events(){
+         name =
+         time = 
+         startDate =
+
+     }
+     public Events(String name, String time,  int room, int numberOfParticipants, String startDate){
+         this.name = name;
+         this.time = time;
+         this. room = room;
+         this.numberOfParticipants = numberOfParticipants;
+         this.startDate = startDate;
+     }
+     public Events(String name, String time, int room, int numberOfParticipants, String startDate){
+         this(name, time, room, numberOfParticipants, start);
+
+
+     }
+// getters and setters
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public int getNumberOfParticipants() {
+        return this.numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
 }
+      
