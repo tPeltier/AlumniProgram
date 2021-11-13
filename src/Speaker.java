8dd602fd6e2b8a7efe46980c3aa74b37e9ace2b6
Army4Life23 <93214458+Army4Life23@users.noreply.github.com>
2021-11-13 14:41:17 -0500

    Speaker Class

---
+++ src/Speaker.java
@@ -0,0 +1,58 @@
+package src;
+
+public class Speaker {
+    String name;
+    String topic;
+    int phoneNumber;
+    String emailAddress;
+    Object untitledField;
+
+    public Speaker(String name, String topic, int phoneNumber, String emailAddress, Object untitledField) {
+        this.name = name;
+        this.topic = topic;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.untitledField = untitledField;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public Object getUntitledField() {
+        return untitledField;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setUntitledField(Object untitledField) {
+        this.untitledField = untitledField;
+    }
+    
+}
