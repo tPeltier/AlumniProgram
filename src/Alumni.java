8a5132a1461c434acaba7469f310d5fbf80ccbc1
slater58 <aslater3122@gmail.com>
2021-11-13 15:40:30 -0500

    Co-authored-by: squidlord99 <squidlord99@users.noreply.github.com> Co-authored-by: tPeltier <tPeltier@users.noreply.github.com>

--- src/Alumni.java
+++ src/Alumni.java
@@ -1,3 +1,87 @@
 public class Alumni {
+    private String name;
+    private String address;
+    private String major;
+    private String gradYear;
+    private String job;
+    private String organization;
+
+    public Alumni() {
+
+    }
+    /**
+     * passing in values for alumni
+     * @param name name of alumni
+     * @param address mailing address
+     * @param major School Major for alumni's degree
+     * @param gradYear Year of graduation 
+     * @param job employment of alumni
+     * @param organization employer of almuni
+     */
+    public Alumni(String name, String address, String major, String gradYear, String job, String organization) {
+        this.name = name;
+        this.address = address;
+        this.major = major;
+        this.gradYear = gradYear;
+        this.job = job;
+        this.organization = organization;
+    }
+
+    /**
+     * to get the name of alumni
+     * @return returns the name of alumni 
+     */ 
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * sets name of alumni
+     * @param name passing name of alumni
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMajor() {
+        return this.major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGradYear() {
+        return this.gradYear;
+    }
+
+    public void setGradYear(String gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public String getJob() {
+        return this.job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getOrganization() {
+        return this.organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
     
+
 }
