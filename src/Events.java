a8b2c51c0b10ffc9f380c6e686b125580086a5bf
justicepeltier <blackeagle393@gmail.com>
2021-11-15 17:05:25 -0500

    added id number to events

--- src/Events.java
+++ src/Events.java
@@ -1,28 +1,36 @@
+import java.util.ArrayList;
 
 public class Events {
 
-    String name;
-    String time;
-    int room;
-    int numberOfParticipants;
-    String startDate;
+    private static int eventId = 0000;
+    private int id;
+    private String name;
+    private String time;
+    private int room;
+    private int numberOfParticipants;
+    private String startDate;
+    private ArrayList<String> attendants;
 
+    // constructors
+    public Events() {
 
+    }
 
-    // constructors 
-    public Events(){
+    public Events(String name, String time, int room, int numberOfParticipants, String startDate) {
+        this.id = eventId;
+        this.name = name;
+        this.time = time;
+        this.room = room;
+        this.numberOfParticipants = numberOfParticipants;
+        this.startDate = startDate;
+        this.attendants = new ArrayList<>();
+    }
 
-     }
-     public Events(String name, String time,  int room, int numberOfParticipants, String startDate){
-         this.name = name;
-         this.time = time;
-         this. room = room;
-         this.numberOfParticipants = numberOfParticipants;
-         this.startDate = startDate;
-     }
+    // getters and setters
+    public int id() {
+        return this.id;
+    }
 
-
-// getters and setters
     public String getName() {
         return this.name;
     }
@@ -64,4 +72,3 @@ public class Events {
     }
 
 }
-      
