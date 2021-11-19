ed12268341e729c2ba3b770a742f93b235044179
Army4Life23 <93214458+Army4Life23@users.noreply.github.com>
2021-11-19 14:31:04 -0500

    Donation Update
    Donations class. Still may need Receipt text file.

--- src/Donation.java
+++ src/Donation.java
@@ -1,7 +1,51 @@
 
+import java.util.Date;
+
+
 public class Donation {
-   
+    
+    private String personName;
+    private String donationTowards;
+    private Double ammountDonated;
+    private java.util.Date dateCreated;
+    //Still need receipt?? 
+
+    
     public Donation() {
         super();
     }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public String getDonationTowards() {
+        return donationTowards;
+    }
+
+    public Double getAmmountDonated() {
+        return ammountDonated;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public void setDonationTowards(String donationTowards) {
+        this.donationTowards = donationTowards;
+    }
+
+    public void setAmmountDonated(Double ammountDonated) {
+        this.ammountDonated = ammountDonated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    
+    
 }
