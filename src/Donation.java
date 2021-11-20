70c62ca1640ceeed7313915c60b96ace67790f36
tPeltier <81177526+tPeltier@users.noreply.github.com>
2021-11-19 22:23:50 -0500

    pushing micheal's code

--- src/Donation.java
+++ src/Donation.java
@@ -1,7 +1,44 @@
+import java.util.Date;
 
 public class Donation {
-   
+    
+    private String personName;
+    private String donationTowards;
+    private Double amountDonated;
+    private java.util.Date dateCreated;
+    //Still need receipt?? 
+
+    
     public Donation() {
-        super();
+        dateCreated = new  Date();
     }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public String getDonationTowards() {
+        return donationTowards;
+    }
+
+    public Double getAmountDonated() {
+        return amountDonated;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public void setDonationTowards(String donationTowards) {
+        this.donationTowards = donationTowards;
+    }
+
+    public void setAmountDonated(Double amountDonated) {
+        this.amountDonated = amountDonated;
+    }
+    
 }
