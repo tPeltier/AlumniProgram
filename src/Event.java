af6761e456fc4e3f435e2dd8df551ffc28eb044d
tPeltier <81177526+tPeltier@users.noreply.github.com>
2021-11-28 04:04:10 -0500

    formatted and added checkforattendance methd

--- src/Event.java
+++ src/Event.java
@@ -20,17 +20,17 @@ public class Event implements CommonMethods {
         // empty
     }
 
-
     // existing event
     /**
      * Constructor for existing Events
-     * @param id Event ID
-     * @param name Event Name
-     * @param room Room where Event is happening
+     * 
+     * @param id          Event ID
+     * @param name        Event Name
+     * @param room        Room where Event is happening
      * @param spotsFilled Number of people participating
-     * @param startDate LocalDateTime information about the Event
-     * @param attendant ArrayList of attending members names
-     * @param host Host object for the host of the Event
+     * @param startDate   LocalDateTime information about the Event
+     * @param attendant   ArrayList of attending members names
+     * @param host        Host object for the host of the Event
      */
     public Event(int id, String name, int room, int totalSpots, LocalDateTime startDate,
             ArrayList<String> attendant, Host host) {
@@ -48,12 +48,13 @@ public class Event implements CommonMethods {
     // adding event
     /**
      * Constructor for adding Events
-     * @param id Event ID
-     * @param name Event Name
-     * @param room Room where Event is happening
+     * 
+     * @param id          Event ID
+     * @param name        Event Name
+     * @param room        Room where Event is happening
      * @param spotsFilled Number of people participating
-     * @param startDate LocalDateTime information about the Event
-     * @param host Host object for the host of the Event
+     * @param startDate   LocalDateTime information about the Event
+     * @param host        Host object for the host of the Event
      */
     public Event(int id, String name, int room, int totalSpots, LocalDateTime startDate, Host host) {
         this.id = id;
@@ -67,22 +68,25 @@ public class Event implements CommonMethods {
 
     /**
      * Get guest speaker obj
+     * 
      * @return Guest speaker Alumni object
      */
-    public Alumni getAlumni(){
+    public Alumni getAlumni() {
         return guestSpeaker;
     }
 
     /**
      * Set the guest speaker
+     * 
      * @param alumni Alumni to be guest speaker
      */
-    public void setAlumni(Alumni alumni){
+    public void setAlumni(Alumni alumni) {
         guestSpeaker = alumni;
     }
 
     /**
      * Get ID of Event
+     * 
      * @return ID of Event
      */
     public int getID() {
@@ -91,6 +95,7 @@ public class Event implements CommonMethods {
 
     /**
      * Gets ID of Host
+     * 
      * @return Host ID
      */
     public int getHostId() {
@@ -103,6 +108,7 @@ public class Event implements CommonMethods {
 
     /**
      * Gets name of Event
+     * 
      * @return Event name
      */
     public String getName() {
@@ -111,15 +117,16 @@ public class Event implements CommonMethods {
 
     /**
      * Set name of Event
+     * 
      * @param name Name of Event
      */
     public void setName(String name) {
         this.name = name;
     }
 
-
     /**
      * Get the year that the Event is occurring in
+     * 
      * @return Year that the Event is held in
      */
     public int getYear() {
@@ -128,6 +135,7 @@ public class Event implements CommonMethods {
 
     /**
      * Get the month that the Event is occurring in
+     * 
      * @return Month that the Event is held in
      */
     public int getMonth() {
@@ -135,7 +143,8 @@ public class Event implements CommonMethods {
     }
 
     /**
-     * Get the day that the Event is occurring on 
+     * Get the day that the Event is occurring on
+     * 
      * @return Day that the Event is held on
      */
     public int getDay() {
@@ -144,6 +153,7 @@ public class Event implements CommonMethods {
 
     /**
      * Get the hour that the Event starts
+     * 
      * @return Hour that the Event starts
      */
     public int getHour() {
@@ -152,6 +162,7 @@ public class Event implements CommonMethods {
 
     /**
      * Get the minute that the Event starts
+     * 
      * @return Minute that the Event starts
      */
     public int getMinute() {
@@ -160,10 +171,11 @@ public class Event implements CommonMethods {
 
     /**
      * Set the date / time information for the Event
-     * @param year Year of Event
-     * @param month Month of Event
-     * @param day Day of Event
-     * @param hour Starting hour of Event
+     * 
+     * @param year   Year of Event
+     * @param month  Month of Event
+     * @param day    Day of Event
+     * @param hour   Starting hour of Event
      * @param minute Starting minute of Event
      */
     public void setTime(int year, int month, int day, int hour, int minute) {
@@ -178,6 +190,7 @@ public class Event implements CommonMethods {
 
     /**
      * Get room number of Event
+     * 
      * @return Room number of Event
      */
     public int getRoom() {
@@ -186,6 +199,7 @@ public class Event implements CommonMethods {
 
     /**
      * Set room number of event
+     * 
      * @param room Room Number of Event
      */
     public void setRoom(int room) {
@@ -194,6 +208,7 @@ public class Event implements CommonMethods {
 
     /**
      * Gets number of participants for Event
+     * 
      * @return Number of participants for the Event
      */
     public int getSpotsFilled() {
@@ -202,6 +217,7 @@ public class Event implements CommonMethods {
 
     /**
      * Set number of participants for event
+     * 
      * @param spotsFilled Number of participants for the Event
      */
     public void setSpotsFilled() {
@@ -211,7 +227,7 @@ public class Event implements CommonMethods {
     public int getTotalSpots() {
         return this.totalSpots;
     }
-    
+
     public void setTotalSpots(int totalSpots) {
         this.totalSpots = totalSpots;
     }
@@ -227,27 +243,45 @@ public class Event implements CommonMethods {
 
     /**
      * Add an attendants name to attending arrayList
+     * 
      * @param name Name of attending Alumni
      */
     public void addAttendant(String name) {
         attendants.add(name);
     }
 
+    public boolean checkForAttendance(String name) {
+        boolean att = false;
+        for (int i = 0; i < attendants.size(); i++) {
+            if (name.equals(attendants.get(i))) {
+                att = true;
+            } else
+                att = false;
+        }
+        return att;
+    }
+
     /**
      * Format DateTime information to be human readable
+     * 
      * @return DateTime info in human readable format
      */
     public String formatDateTime() {
-        return startDate.getMonthValue() + "-" + startDate.getDayOfMonth() + "-" + startDate.getYear() + " at " + startDate.getHour() + ":" + startDate.getMinute();
+        return startDate.getMonthValue() + "-" + startDate.getDayOfMonth() + "-" + startDate.getYear() + " at "
+                + startDate.getHour() + ":" + startDate.getMinute();
     }
 
     @Override
     public String toString() {
-        return "Event ID: " + getID() + " |" + " Event Name: " + getName() + " |" +  " Event Date and Time: " + formatDateTime() + " |" +  " Event Room Number: " + getRoom() + " |" + " Total Number of Spots: " + getTotalSpots() + " |" + " Number of Spots Open: " + getOpenSpots() + " |" + "Number of Spots Filled: " + getSpotsFilled();
+        return "Event ID: " + getID() + " |" + " Event Name: " + getName() + " |" + " Event Date and Time: "
+                + formatDateTime() + " |" + " Event Room Number: " + getRoom() + " |" + " Total Number of Spots: "
+                + getTotalSpots() + " |" + " Number of Spots Open: " + getOpenSpots() + " |"
+                + "Number of Spots Filled: " + getSpotsFilled();
     }
 
     /**
      * Save the Event's information
+     * 
      * @return Event's information formatted to save to text file
      */
     public String save() {
@@ -256,22 +290,26 @@ public class Event implements CommonMethods {
 
     /**
      * Save the Event's DateTime information
+     * 
      * @return Event's DateTime information formatted to save to text file
      */
     public String saveDateTime() {
-        return startDate.getYear() + "," + startDate.getMonthValue() + "," + startDate.getDayOfMonth() + "," + startDate.getHour() + "," + startDate.getMinute();
+        return startDate.getYear() + "," + startDate.getMonthValue() + "," + startDate.getDayOfMonth() + ","
+                + startDate.getHour() + "," + startDate.getMinute();
     }
 
     /**
      * Save the Event's Host
-     * @return The save() call for the Host 
+     * 
+     * @return The save() call for the Host
      */
     public String saveHost() {
         return host.save();
     }
 
     /**
-     * Save the Event's attendant list 
+     * Save the Event's attendant list
+     * 
      * @return Event's attendant list formatted to save to text file
      */
     public String saveAttendants() {
