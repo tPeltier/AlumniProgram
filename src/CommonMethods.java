768d0b05b7469225d78f78e9d920cbee15e93b71
tPeltier <81177526+tPeltier@users.noreply.github.com>
2021-11-28 08:29:34 -0500

    + javadocs

--- src/CommonMethods.java
+++ src/CommonMethods.java
@@ -1,5 +1,20 @@
 public interface CommonMethods {
+
+    /**
+     * Get Name
+     * @return Name
+     */
     String getName();
+
+    /**
+     * Get ID
+     * @return ID
+     */
     int getID();
+
+    /**
+     * Set Name
+     * @param name New Name
+     */
     void setName(String name);
 }
--- src/InvalidEntry.java
+++ src/InvalidEntry.java
@@ -1,5 +1,9 @@
 public class InvalidEntry extends IllegalArgumentException {
     
+    /**
+     * Invalid Entry Error
+     * @param message Error message to display
+     */
     public InvalidEntry(String message) {
         super(message);
     }
