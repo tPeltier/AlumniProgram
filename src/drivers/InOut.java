d63c514678f5b40b023fd8eb8ac5491161e424d6
tPeltier <81177526+tPeltier@users.noreply.github.com>
2021-12-01 01:13:11 -0500

    simplified boolean returns where possible

--- src/drivers/InOut.java
+++ src/drivers/InOut.java
@@ -892,10 +892,7 @@ public class InOut {
      * @return Whether the ALumni is already attending
      */
     public boolean alreadyAttendingEvent(int id, int eventID) {
-        if (eventMap.get(eventID).checkForAttendance(id))
-            return true;
-        else
-            return false;
+        return (eventMap.get(eventID).checkForAttendance(id));
     }
 
     /**
@@ -906,10 +903,7 @@ public class InOut {
      * @return Whether the Alumni is already attending
      */
     public boolean alreadyAttendingTraining(int id, int trainingID) {
-        if (trainingMap.get(trainingID).checkForAttendance(id))
-            return true;
-        else
-            return false;
+        return (trainingMap.get(trainingID).checkForAttendance(id));
     }
 
     /**
@@ -919,10 +913,7 @@ public class InOut {
      * @return true if Event is in map, false if Event is not in map
      */
     public boolean isExistingEvent(int eventID) {
-        if (eventMap.containsKey(eventID)) {
-            return true;
-        } else
-            return false;
+        return (eventMap.containsKey(eventID));
     }
 
     /**
@@ -932,10 +923,7 @@ public class InOut {
      * @return true if Event is in map, false if Training Event is not in map
      */
     public boolean isExistingTraining(int eventID) {
-        if (trainingMap.containsKey(eventID)) {
-            return true;
-        } else
-            return false;
+        return (trainingMap.containsKey(eventID)); {
     }
     // ==================== CREATE ====================
 
